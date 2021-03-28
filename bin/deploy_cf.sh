09e3ea688398cefeb235aa92c22a725147cdf8b7
Pepijn Looije <p@epij.nl>
2021-03-27 21:30:12 -0300

    Move function name to the cloudbuild file

--- bin/deploy_cf.sh
+++ bin/deploy_cf.sh
@@ -3,13 +3,13 @@
 set -euxo pipefail
 
 function main() {
-  local project_id="$1"
+  local function_name="$1"
+  local project_id="$2"
 
-  gcloud functions deploy --region europe-west2 --runtime java11 --source /workspace/artifact/ "$@" \
+  gcloud functions deploy --region europe-west2 --runtime java11 --source /workspace/artifact/ "$function_name" \
     --service-account "telegram-bot@$project_id.iam.gserviceaccount.com" \
     --trigger-topic "cloud-builds" \
     --entry-point TelegramBot
 }
 
-main "telegram-bot"
-
+main "$@"
