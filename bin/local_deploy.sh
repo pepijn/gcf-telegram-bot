1e84a6794b46e6105ddfac0b174a72659b66197c
Pepijn Looije <p@epij.nl>
2021-03-27 22:42:58 -0300

    Test before deploying locally

--- bin/local_deploy.sh
+++ bin/local_deploy.sh
@@ -8,10 +8,12 @@ shellcheck "$0"
 cd "$(dirname "$(realpath "$0")")/.."
 
 function main() {
+  ./bin/test.sh
+
   local tmp_dir
   tmp_dir="$(gmktemp --directory)"
 
-  ./bin/build.sh SNAPSHOT \
+  ./bin/build.sh "dev-SNAPSHOT" \
     ~/Library/Java/JavaVirtualMachines/adopt-openjdk-11.0.10/Contents/Home \
     "$tmp_dir"
 
